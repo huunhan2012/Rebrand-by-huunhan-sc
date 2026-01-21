1396ddeec5d794cea481c2fb00788e91ca930df7
huunhan2012 <tamnguyen457hcm@gmail.com>
2026-01-21 11:55:45 +0700

    Add huunhan-sc.lua script with notification

---
+++ huunhan-sc.lua
@@ -0,0 +1,10 @@
+-- huunhan sc
+-- Rebrand Hardcore Script
+
+print("Huunhan SC Loaded Successfully!")
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Huunhan SC",
+    Text = "Script Loaded!",
+    Duration = 5
+})
